972da6b9c2809ba7f17323af3e38d04bab137234
Shahbaz Ali <shahbazali078272@gmail.com>
2025-08-03 10:30:49 +0500

    Add files via upload

---
+++ CppInUrduByShabby.h
@@ -0,0 +1,37 @@
+#ifndef UrduVersionBy_SHABBY
+#define UrduVersionBy_SHABBY
+//Includes Header components
+#include <iostream>
+using namespace std;
+#define ShuruKarein int main()
+//Normal Statements like Output and input
+#define Sunain  cin
+#define Sunain cout
+#define AagyChalein endl
+//Conditional Statements
+#define Agar if
+#define NahinTo else
+#define Magar switch
+#define Halaat case
+#define BaakiSB defualt
+//Datatypes
+#define BegairPoint int
+#define BadaNumber double
+#define PointWala float
+#define Huroof char
+#define Lafz string
+// Symbols
+#define barabarNahinHai !=
+#define barabarHay ==
+#define ChotaHai <
+#define BadaHai >
+#define chotaBarabar <=
+#define BadaBarabar >=
+//Loops
+#define KeLiye for
+#define JabTak while
+#define Kro do
+
+//Owners Credit and all Rights resserved
+#define Maalik "\n\n*********************************************\nThis Urdu Version of C++ is created By shabby {Shahbaz Ali (2k25/ITE/117).\n He is currently studing in Department of Information Tecchnoloogya at University of Sindh Jamshoro.\nFor Contribution and support of this work\nDo check it out with this link: https://portfolio-ebfshahbaz01.netlify.app\n*********************************************\n \n "
+#endif // UrduVersionBy_SHABBY
